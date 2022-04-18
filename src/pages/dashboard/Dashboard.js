337c04431c1992db31f15b48b181e4164ab0a955
Sambhav Sharma <devsambhav50@gmail.com>
2022-04-18 15:22:31 +0530

    some changes in css

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -7,7 +7,7 @@ import ProjectList from "../../components/ProjectList";
 const Dashboard = () => {
   const { documents: projects, error } = useCollection("projects");
   return (
-    <div>
+    <div className="dashboard">
       <h2 className="page-title">Dashboard</h2>
       {error && <p className="error">{error}</p>}
       {projects && <ProjectList projects={projects} />}
