95c9a2a21cbe9f65d247f3c3a2191dba14c57f98
Sambhav Sharma <devsambhav50@gmail.com>
2022-05-12 23:06:12 +0530

    change in login UI

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -5,6 +5,8 @@ import { useAuthContext } from "../../hooks/useAuthContext";
 
 // styles
 import "./Login.css";
+import GoogleIcon from "../../assets/google-icon.svg";
+import FacebookIcon from "../../assets/facebook-icon.svg";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -27,34 +29,49 @@ const Login = () => {
   }, [user, history]);
 
   return (
-    <form onSubmit={handleSubmit} className="auth-form">
-      <h2>Login</h2>
-      <label>
-        <span>Email:</span>
-        <input
-          type="email"
-          onChange={(e) => setEmail(e.target.value)}
-          required
-          value={email}
-        />
-      </label>
-      <label>
-        <span>Password:</span>
-        <input
-          type="password"
-          onChange={(e) => setPassword(e.target.value)}
-          required
-          value={password}
-        />
-      </label>
-      {isLoading && (
-        <button className="btn" disabled>
-          Logging...
-        </button>
-      )}
-      {!isLoading && <button className="btn">Login</button>}
-      {error && <div className="error">{error}</div> }
-    </form>
+    <>
+      <form onSubmit={handleSubmit} className="auth-form auth-login">
+        <h2>Login</h2>
+        <label>
+          <span>Email:</span>
+          <input
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            value={email}
+          />
+        </label>
+        <label>
+          <span>Password:</span>
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            value={password}
+          />
+        </label>
+        {isLoading && (
+          <button className="btn" disabled>
+            Logging...
+          </button>
+        )}
+        {!isLoading && <button className="btn">Login</button>}
+        {error && <div className="error">{error}</div>}
+      </form>
+      <div className="sign-in-box">
+        <h3 className="sign-text">Sign In With</h3>
+        <div className="sign-in">
+          <button className="btn google-btn">
+            <img src={GoogleIcon} alt="google-icon" />
+            <p>Google</p>
+          </button>
+          <button className="btn facebook-btn">
+            <img src={FacebookIcon} alt="facebook-icon" />
+            <p>Facebook</p>
+          </button>
+        </div>
+      </div>
+    </>
   );
 };
 
